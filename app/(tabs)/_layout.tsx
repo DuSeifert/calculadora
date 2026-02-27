5baa0552c7d008f64807a7722b6893b24300fba2
DuSeifert <eduardoseifertes@gmail.com>
2026-02-27 00:05:06 -0300

    lógica dos operadores

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,61 @@
 import { useState } from "react";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { Double } from "react-native/Libraries/Types/CodegenTypes";
 
 export default function TabLayout() {
 
-  function MyButton() {const [count, setCount] = useState(0);  
+  //TODO: Lógica da calculadora pronta, só falta fazer a lógica para mostrar a expressão no display enquanto digita
+  //TODO: Adicionar a funcionalidade de porcentagem
 
+  const [valor, setValor] = useState("");
+  const [anterior, setAnterior] = useState("");
+  const [operador, setOperador] = useState("");
+  const [display, setDisplay] = useState(" ");
+
+  const buttonPress = (num: string) => {
+    if (display == "0" || display == " ") {
+      setDisplay(num.toString());
+    } else {
+      setDisplay(display + num.toString());
+    };
+  }
+
+  const buttonOP = (op: string) => {
+      setAnterior(display);
+      setDisplay(" ");
+      setOperador(op);
+  }
+
+  const buttonIgual = () => {
+    let resultado: Double = 0;
+    switch (operador) {
+      case "+":
+        resultado = parseFloat(anterior) + parseFloat(display);
+        break;
+      case "-":
+        resultado = parseFloat(anterior) - parseFloat(display);
+        break;
+      case "*":
+        resultado = parseFloat(anterior) * parseFloat(display);
+        break;
+      case "/":
+        resultado = parseFloat(anterior) / parseFloat(display);
+        break;
+    }
+    setDisplay(resultado.toString());
+  }
+  
+  const buttonClear = () => {
+    setValor("");
+    setAnterior("");
+    setOperador("");
+    setDisplay(" ");
+  }
+
+  const backspace = () => {
+    setDisplay(display.slice(0, -1));
+  }
 
   return (
     <>
@@ -13,17 +63,17 @@ export default function TabLayout() {
 
       <View style={styles.container}>  
         <View style={styles.painel}>
-          <Text style={styles.textoPainel}>resultado</Text>
+          <Text style={styles.textoPainel}>{display}</Text>
         </View>
 
         <View style={styles.areaBotao}>
-          <TouchableOpacity style={styles.botaoAC}>
+          <TouchableOpacity onPress = {() => buttonClear()} style={styles.botaoAC}>
             <Text style={styles.textoBotao}>AC</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.botaoOP}>
             <Text style={styles.textoBotao}>%</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botaoOP}>
+          <TouchableOpacity onPress = {() => buttonOP("/")} style={styles.botaoOP}>
             <Text style={styles.textoBotao}>/</Text>
           </TouchableOpacity>
           
@@ -31,61 +81,61 @@ export default function TabLayout() {
 
         <View style={styles.areaBotao}>
 
-          <TouchableOpacity style={styles.botao}>
+          <TouchableOpacity onPress={() => buttonPress("7")} style={styles.botao}>
             <Text style={styles.textoBotao}>7</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botao}>
+          <TouchableOpacity onPress={() => buttonPress("8")} style={styles.botao}>
             <Text style={styles.textoBotao}>8</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botao}>
+          <TouchableOpacity onPress={() => buttonPress("9")} style={styles.botao}>
             <Text style={styles.textoBotao}>9</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botaoOP}>
+          <TouchableOpacity onPress = {() => buttonOP("*")} style={styles.botaoOP}>
             <Text style={styles.textoBotao}>*</Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.areaBotao}>
-          <TouchableOpacity style={styles.botao}>
+          <TouchableOpacity onPress={() => buttonPress("4")} style={styles.botao}>
             <Text style={styles.textoBotao}>4</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botao}>
+          <TouchableOpacity onPress={() => buttonPress("5")} style={styles.botao}>
             <Text style={styles.textoBotao}>5</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botao}>
+          <TouchableOpacity onPress={() => buttonPress("6")} style={styles.botao}>
             <Text style={styles.textoBotao}>6</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botaoOP}>
+          <TouchableOpacity onPress = {() => buttonOP("-")} style={styles.botaoOP}>
             <Text style={styles.textoBotao}>-</Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.areaBotao}>
-          <TouchableOpacity style={styles.botao}>
+          <TouchableOpacity onPress={() => buttonPress("1")} style={styles.botao}>
             <Text style={styles.textoBotao}>1</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botao}>
+          <TouchableOpacity onPress={() => buttonPress("2")} style={styles.botao}>
             <Text style={styles.textoBotao}>2</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botao}>
+          <TouchableOpacity onPress={() => buttonPress("3")} style={styles.botao}>
             <Text style={styles.textoBotao}>3</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botaoOP}>
+          <TouchableOpacity onPress = {() => buttonOP("+")} style={styles.botaoOP}>
             <Text style={styles.textoBotao}>+</Text>
           </TouchableOpacity>
         </View>
 
         <View style={styles.areaBotao}>
-          <TouchableOpacity style={styles.botao}>
+          <TouchableOpacity onPress={() => buttonPress("0")} style={styles.botao}>
             <Text style={styles.textoBotao}>0</Text>
           </TouchableOpacity>
           <TouchableOpacity style={styles.botao}>
             <Text style={styles.textoBotao}>.</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botao}>
-            <Text style={styles.textoBotao}> Bs </Text>
+          <TouchableOpacity onPress = {() => backspace()}style={styles.botao}>
+            <Text style={styles.textoBotao}>←</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.botaoIgual}>
+          <TouchableOpacity onPress = {() => buttonIgual()} style={styles.botaoIgual}>
             <Text style={styles.textoBotao}>=</Text>
           </TouchableOpacity>
 
@@ -107,7 +157,7 @@ const styles = StyleSheet.create({
     
   },
   painel: {
-    backgroundColor: "gray",
+    backgroundColor: "darkslategrey",
     height: 50,
     marginTop: 50,
     margin: 10,
@@ -122,7 +172,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   botao: {
-    backgroundColor: "grey",
+    backgroundColor: "dimgray",
     padding: 10,
     borderRadius: 5,
     margin: 10,
@@ -145,7 +195,7 @@ const styles = StyleSheet.create({
 
   },
   botaoIgual: {
-    backgroundColor: "yellow",
+    backgroundColor: "red",
     padding: 10,
     borderRadius: 5,
     margin: 10,
